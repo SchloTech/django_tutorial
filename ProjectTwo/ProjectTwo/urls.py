353e298b8a3d96f0bf9247b736f8441616e86b9c
Jake Schlottag <jakeschlottag@Jakes-M1-MBP.localdomain>
2023-01-05 07:37:20 -0500

    tutorial project & app + challenge project & app

--- ProjectTwo/ProjectTwo/urls.py
+++ ProjectTwo/ProjectTwo/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""ProjectTwo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from first_app import views
+from AppTwo import views
 
 urlpatterns = [
     path('', views.index, name='index'),
