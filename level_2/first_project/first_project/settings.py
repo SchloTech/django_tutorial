79b1ac725dd57cde9ac08a99caeef436b76a22b0
Jake Schlottag <jakeschlottag@Jakes-M1-MBP.localdomain>
2023-01-23 22:47:29 -0500

    add models

--- level_2/first_project/first_project/settings.py
+++ level_2/first_project/first_project/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_DIR = os.path.join(BASE_DIR, "static") # add this line
+STATIC_DIR = os.path.join(BASE_DIR, "static")  # add this line
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'first_app',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [   #add this list
+STATICFILES_DIRS = [  # add this list
     STATIC_DIR,
 ]
 
