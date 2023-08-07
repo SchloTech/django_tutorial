f9e481491f738209ed18e7388b5aa7ee25c221e8
Jake Schlottag <jakeschlottag@Jakes-M1-MBP.localdomain>
2023-08-07 07:13:19 -0400

    many to many relationships

--- learning_blocks/book_store/book_outlet/models.py
+++ learning_blocks/book_store/book_outlet/models.py
@@ -4,6 +4,16 @@ from django.urls import reverse
 from django.utils.text import slugify 
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
@@ -38,6 +48,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, blank=True, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
